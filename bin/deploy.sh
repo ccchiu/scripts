c86004faf767ec21d4cab60c1f21c628ee820bdf
JerryChiu <jerry_chiu@icpdas.com>
2009-12-07 17:43:12 +0800

    add git tag name

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -4,6 +4,8 @@ DIR=/home/jerry/tmp/build_out
 WORK_DIR=/home/jerry/WORK/pdsall.git
 GIT_EXPORT=$HOME/bin/git_export
 WWW=/LIN_WWW
+BUILD_TIME=`date +%Y%m%d_%H%M%S`;
+GIT_V=`git log --pretty=format:'%h' |head -1`
 export WORK_DIR
 
 if [ "$PWD" != "$WORK_DIR" ]; then
@@ -26,6 +28,8 @@ echo "Copy to Lin_WWW";
 cp -Rf $DIR/web/* $WWW
 sed -e  's/define(\"DEV\",1);/define(\"DEV\",0);/g' -e 's/define(\"PRODUCTION\",0);/define(\"PRODUCTION\",1);/g' $WWW/config.php > /tmp/ddd.l
 
+sed -e 's/bbbst/'$BUILD_TIME'('$GIT_V')/g' $WWW/index.html > /tmp/iii.l
+cat /tmp/iii.l > $WWW/index.html
 cat /tmp/ddd.l > $WWW/config.php
 
 chmod -R 777 $WWW/db
